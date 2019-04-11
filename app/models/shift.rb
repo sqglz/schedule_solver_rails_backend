dc39c5b72d9a3a5bebcee3c4989f387f900e6c50
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-04-10 19:04:23 -0600

    creates a couple new instance methods on Shift model

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -8,6 +8,14 @@ class Shift < ApplicationRecord
     shift_responsibilities
   end
 
+  def filled?
+    !shifts.pluck(:assigned).include?(false)
+  end
+
+  def unfilled_assignments
+    shifts.where(assigned: false).pluck(:name)
+  end
+
   def assign_shift(user, responsibility, assignment=nil)
     return false unless user.role? == 'worker' || user.role? == 'manager'
 
@@ -17,7 +25,7 @@ class Shift < ApplicationRecord
       respo = responsibilities.select{|r| r.name == assignment.downcase.capitalize }
       assignment = shift_responsibilities.find_by(responsibility_id: respo.id)
     end
-    
+
     assignment.user_id = user.id
     assignment.save
     return true
