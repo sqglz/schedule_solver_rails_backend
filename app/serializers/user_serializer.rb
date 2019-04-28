7e2bcb17f16ce07b42b511b9155391ae2fd8f3a5
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-04-28 16:15:01 -0600

    fixes UserSerializer and adds an index to UserController

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :employment_start_date, :days_employed
+  attributes  :first_name, :last_name, :employment_start_date, :days_employed,
+              :email
 
   def days_employed
-    (DateTime.now.to_date - object.employment_start_date.to_date).to_i
+    if !object.employment_start_date
+      (DateTime.now.to_date - object.employment_start_date&.to_date.to_i)
+    else
+      DateTime.now.to_date - object.employment_start_date&.to_date
+    end
   end
 end
