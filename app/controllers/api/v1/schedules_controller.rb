f7ec4bf47389838c2ba5deb1ed4eeb41fc6b44ac
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-04-28 17:19:03 -0600

    bug on schedules_controller

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -2,16 +2,4 @@ class Api::V1::SchedulesController < ApplicationController
   def index
     render json: Schedule.all
   end
-
-  def show
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
 end
